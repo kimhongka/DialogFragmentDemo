1909aed626e2ee60ceca1335e53621a399c07763
honggyu kim <kimhongka123@gmail.com>
2016-10-24 11:21:13 +1000

    initial

---
+++ DialogFragmentDemo/DialogFragmentAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DialogFragmentDemo
+{
+    public class DialogFragmentAdapter : DialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle);
+
+            var view = inflater.Inflate(Resource.Layout.DialogFragmentContent, container, false);
+
+            Button closeButton = view.FindViewById<Button>(Resource.Id.CloseButton);
+            closeButton.Click += delegate {
+                Dismiss();
+            };
+
+            return view;
+        }
+       
+
+    }
+}
+
---
+++ DialogFragmentDemo/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DialogFragmentDemo
+{
+    [Activity(Label = "DialogFragmentDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button popupDialogButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            SetContentView(Resource.Layout.Main);
+            popupDialogButton = FindViewById<Button>(Resource.Id.Dialog_Button);
+            popupDialogButton.Click += OnPopupWinowButtonClicked;
+        }
+
+        private void OnPopupWinowButtonClicked(object sender, EventArgs e)
+        {
+            popupDialogButton.Enabled = false;
+
+            FragmentTransaction transaction = FragmentManager.BeginTransaction();
+            DialogFragmentAdapter dialogFragmentAdapter = new DialogFragmentAdapter();
+            dialogFragmentAdapter.Show(transaction, "testTag");
+            popupDialogButton.Enabled = true;
+        }
+    }
+}
+
